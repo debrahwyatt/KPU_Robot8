962cbe9c8513aeb9a5837b67ce437348abfaeccf
ededward <eddyedwardwu7882@gmail.com>
2017-03-16 23:38:17 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -8,17 +8,19 @@
 // main acts as a cyclical task sequencer
 void main(void)
 {
-   
+    
     initialization(); // function from sumovore.c
                       // it sets up pwm (using timer2),
                       // IO pins, the ADC, the 
                       // USART and the default
                       // threshold
-    printf("\n\rKwantlen APSC1299 simple curve follower -- with error codes\n\r"); 
+    printf("\n\rKwantlen APSC1299 simple curve follower "
+            "-- with error codes\n\r"); 
     ClrWdt();         // defined in <p18f4525.h>
 
-//  threshold = 450u; // to change from default value
-                     // uncomment and change to any unsigned int <1024u -- most usually <512u
+  
+                     // uncomment and change to any 
+                     // unsigned int <1024u -- most usually <512u
 
     while(1)
     {
@@ -29,6 +31,7 @@ void main(void)
 	                    // a different purpose change this line
 	                    // and make your own LED setting function
         motor_control();    // function from motor_control.c 
+        
         ClrWdt();           // defined in <p18f4525.h>
         if(lvd_flag_set())  LVtrap();
     }
